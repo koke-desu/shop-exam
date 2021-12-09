abc45eecdf15d20c9f80753f9b8a44516a447bda
koke-desu <gbpvp25645@yahoo.co.jp>
2021-12-09 20:37:11 +0900

    headerのデザインを更新

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,27 +3,40 @@ import { Link } from "react-router-dom";
 import { MdSearch as SearchIcon } from "react-icons/md";
 import { MdMode as CreateIcon } from "react-icons/md";
 import { MdList as ManageIcon } from "react-icons/md";
+import { MdAccountCircle as UserIcon } from "react-icons/md";
 
 type Props = {};
 
 // Top、探す、つくる、管理する、へのリンクの設置。余裕があればmodalを使ったHelpを作れば完成度が上がりそう。
 const Header: React.VFC<Props> = ({}) => {
   return (
-    <div className="w-full h-24 bg-indigo-700 flex justify-center items-center">
-      <div className="container flex flex-row justify-start items-center flex-nowrap">
-        <div className="w-36 h-16 bg-white">かっこいいロゴ</div>
-        <nav className="h-full w-96">
-          <ul className="flex flex-row">
-            <NavButton title="さがす" icon={<SearchIcon fill="#fff" width={48} />} link="/list" />
-            <NavButton title="つくる" icon={<CreateIcon fill="#fff" width={48} />} link="/create" />
+    <div className="w-full h-20 bg-header flex justify-center items-center">
+      <div className="container h-full flex flex-row justify-end items-center flex-nowrap">
+        <div className="w-36 h-16 mr-auto bg-white flex justify-center items-center">
+          かっこいいロゴ
+        </div>
+        <nav className="w-96 mr-8">
+          <ul className="flex flex-row border-r-2 border-black">
+            <NavButton
+              title="さがす"
+              icon={<SearchIcon fill="#fff" opacity={0.5} size={48} />}
+              link="/list"
+            />
+            <NavButton
+              title="つくる"
+              icon={<CreateIcon fill="#fff" opacity={0.5} size={48} />}
+              link="/create"
+            />
             <NavButton
               title="管理する"
-              icon={<ManageIcon fill="#fff" width={48} />}
+              icon={<ManageIcon fill="#fff" opacity={0.5} size={48} />}
               link="/manage"
             />
           </ul>
         </nav>
-        <div></div>
+        <div className="">
+          <UserIcon size={64} fill="white" />
+        </div>
       </div>
     </div>
   );
@@ -36,12 +49,11 @@ const NavButton: React.VFC<{ title: string; icon: React.ReactNode; link: string 
   link,
 }) => {
   return (
-    <li className="w-1/3">
+    <li className="w-1/3 h-20 duration-500 hover:bg-white hover:bg-opacity-20">
       <Link to={link}>
-        <div className="h-full flex flex-col justify-start items-center">
-          {/* <img src={`images/${fileName}.svg`} color="#fff" width={48} /> */}
+        <div className="h-full flex flex-col justify-start items-center border-l-2 border-gray-900">
           {icon}
-          <p className="text-white">{title}</p>
+          <p className="text-white opacity-50 text-base -mt-2">{title}</p>
         </div>
       </Link>
     </li>
