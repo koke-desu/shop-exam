9e86dd2952da5eb2275457e077085daa94e186ce
koke-desu <gbpvp25645@yahoo.co.jp>
2021-12-14 23:50:43 +0900

    Revert "HPの値が整数になるように修正"
    This reverts commit 33c78a49ffc5c13c010a5b27e33a2d5f9e861f0e.

--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 type Props = {};
 
-// TRPGとはなにか。このWebアプリでは何が出来るかを説明。ItemListなどへのリンクを設置
-// LPみたいな感じで。
 const TopPage: React.VFC<Props> = ({}) => {
   return <div></div>;
 };
